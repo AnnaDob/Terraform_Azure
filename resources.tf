afeed75079de43bc13ce96a5744dfd2e793dd9a5
adobr <annadobrycheva@gmail.com>
2022-04-20 22:21:57 +0300

    creating resource group and storage account

--- resources.tf
+++ resources.tf
@@ -1,20 +1,20 @@
 locals {
-  setup_name = "terra"
+  setup_name = "dev"
 }
 
 
 // Configure the Resource Group
-resource "azurerm_resource_group" "terra_resource_group" {
-  name     = "${local.setup_name}_resource_group"
+resource "azurerm_resource_group" "main" {
+  name     = "${local.setup_name}_main_resource_group"
   location = "West Europe"
 }
 
 // Configure the Storage Account
-resource "azurerm_storage_account" "terra_storageaccount" {
-  name                     = "${local.setup_name}storageaccount2222"
-  resource_group_name      = azurerm_resource_group.terra_resource_group.name
+resource "azurerm_storage_account" "dev" {
+  name                     = "${local.setup_name}storage358"
+  resource_group_name      = azurerm_resource_group.main.name
 
-  location                 = azurerm_resource_group.terra_resource_group.location
+  location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
