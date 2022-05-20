7a9e86ffe59e5774aed6a9f1040b1ed61cc27b00
AnnaDob <annadobrycheva@gmail.com>
2022-05-20 12:02:54 +0300

    added identity to app service

--- app_service.tf
+++ app_service.tf
@@ -12,11 +12,21 @@ resource "azurerm_linux_web_app" "my_app_service" {
   location            = var.location_app_seevice
   service_plan_id     = azurerm_service_plan.my_app_service_plan.id
 
+  identity {
+    type  = "SystemAssigned"
+  }
+
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_key_vault_secret.docker_registry_server_passwd.value
+    DOCKER_REGISTRY_SERVER_URL = format("%s.azurecr.io", azurerm_container_registry.acr.name)
+    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.name
+  }
+
   site_config {
 
     application_stack {
-    docker_image = format("%s.azurecr.io/container_for_image", azurerm_container_registry.acr.name)
-    docker_image_tag = "latest"
+      docker_image = format("%s.azurecr.io/container_for_image", azurerm_container_registry.acr.name)
+      docker_image_tag = "latest"
   }
  }
 }
